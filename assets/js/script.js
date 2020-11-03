b4a955cee92d739f3d853ab80b8cec481fa1a1d3
laurieftn <laurie.ftn@gmail.com>
2020-11-03 14:25:05 +0100

    Modification du fichier .js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,7 @@ document.getElementById('button_valider').onclick = showAlert;
 function showAlert(){
 var pointure = document.getElementById("pointure").value;
 var naissance = document.getElementById("naissance").value;
-var result = (pointure*2);
-var result2 = (result+5);
-var result3 = (result2*50);
-var result4 = (result3-naissance);
-var result5 = (result4+1769);
+var result = (((((pointure*2)+5)*50)-naissance)+1769);
 
-alert('Résultat : ' + result5)
+alert('Résultat : ' + result)
 }
